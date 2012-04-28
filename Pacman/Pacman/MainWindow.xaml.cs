bd869c877c5114a01840c9c199a09e1acbef3a79
Prof-MAG <alexander@molodih.org.ua>
2012-04-28 01:57:14 -0700

    Pacman

---
+++ Pacman/Pacman/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Pacman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pacman.Size = 20;
+        }
+    }
+}
---
+++ Pacman/Pacman/PacmanControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Pacman
+{
+    public class PacmanControl : Control
+    {
+        public static readonly DependencyProperty MouseAngleProperty =
+            DependencyProperty.Register("MouseAngle", typeof(int), typeof(PacmanControl),
+                new PropertyMetadata(default(int)));
+
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register("Size", typeof(double), typeof(PacmanControl),
+                new PropertyMetadata(default(double)));
+
+        public int MouseAngle
+        {
+            get { return (int)GetValue(MouseAngleProperty); }
+            set { SetValue(MouseAngleProperty, value); }
+        }
+
+        public double Size
+        {
+            get { return (double)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+    }
+}
